355488ca2d5ab25fe165a0e8a252fd658840ff1b
Matt <matthew.j.alioto@gmail.com>
2022-05-16 17:47:14 -0400

    Solution

--- letsdrill.rb
+++ letsdrill.rb
@@ -1,19 +1,18 @@
 def get_letter_grade(integer)
- 
   #Put your code here!
-
+  grades = {90 => "A", 80 => "B", 70 => "C", 60 => "D", 0 => "F"}
+  grades.each {|key, value| return value if integer >= key }
 end
 
 def shortest_string(array)
-
   #Put your code here!
-
+  array.sort_by(&:length)[0]
 end
- 
+
 
 
 ### Don't touch anything below this line ###
- 
+
 p "Fetch Letter Grade: You should have 2 trues"
 p get_letter_grade(89) == "B" 
 p get_letter_grade(73) == "C" 
